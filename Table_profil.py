03f51820f1b7ef2c8d2d6bb2f22da622af8030a5
Zahra Dubeau <zdub@dubz.com>
2020-06-29 11:14:02 +0200

    Create user profil table & html

--- Table_profil.py
+++ Table_profil.py
@@ -1,21 +1,44 @@
 drop_user_profil = """
-                    DROP TABLE IF EXISTS profils_usager CASCADE;
+                    DROP TABLE IF EXISTS profil CASCADE;
                     """
 
 user_profil = """
-                CREATE TABLE IF NOT EXISTS profils_usager (
+                CREATE TABLE IF NOT EXISTS profil (
                     id_profil SERIAL PRIMARY KEY,
-                    profils VARCHAR(20)
+                    profil VARCHAR(80)
                 )"""
 
 insert_user_profil = """
-                INSERT INTO profils_usager (profils)
-                VALUES (%(profils)s) 
-                returning id_profil;
-                """
+                    INSERT INTO profil (profil)
+                    VALUES (%(profil)s) 
+                    returning id_profil;
+                    """
 
-delete_user_profil_with_id = """
-                            DELETE 
-                            FROM profils_usager 
+select_user_profil = """
+                    SELECT id_profil as id, profil as profil_usagers, '' as modifier, '' as supprimer
+                    FROM profil; 
+                    """
+
+select_user_profil_with_id = """
+                            SELECT id_profil, profil
+                            FROM profil
                             WHERE id_profil=%s; 
                             """
+
+select_user_profil_with_description = """
+                                    SELECT * 
+                                    FROM profil
+                                    WHERE profil=%(profil)s
+                                    """
+
+update_user_profil = """
+                    UPDATE profil
+                    SET profil=%s 
+                    WHERE id_profil=%s;
+                    """
+
+delete_user_profil = """
+                    DELETE 
+                    FROM profil
+                    WHERE id_profil=%s; 
+                    """

37801ea25c10cc7e013f7400c0d40479faf70de7
Zahra Dubeau <zdub@dubz.com>
2020-07-09 11:49:34 +0200

    Fix bug

--- DB_Initialisation.py
+++ DB_Initialisation.py
@@ -20,32 +20,32 @@ from DB_Connexion import DB_connexion
 # _____________________________________________
 
 full_actions_list = (
-    Table_admin.drop_admin,
-    Table_Apidae.drop_apidae,
     Table_message.drop_message,
     Table_project.drop_project,
+    Table_selection.drop_selection,
+    Table_extraction.drop_selection_extraction,
+    Table_Apidae.drop_apidae,
+    Table_admin.drop_admin,
     Table_category.drop_category,
     Table_profil.drop_user_profil,
-    Table_selection.drop_selection,
+    Table_elementReference.drop_elementRef,
+    Table_relation_selection_category.drop_relation_selection_category,
+    Table_relation_selection_profil.drop_relation_selection_profil,
     Table_ManualEntry.drop_manualEntry,
     Table_freshness.drop_freshness_level,
-    Table_elementReference.drop_elementRef,
-    Table_extraction.drop_selection_extraction,
-    Table_relation_selection_profil.drop_relation_selection_profil,
-    Table_relation_selection_category.drop_relation_selection_category,
-    Table_admin.admin,
-    Table_Apidae.apidae,
     Table_message.message,
     Table_project.project,
+    Table_selection.selection,
+    Table_extraction.selection_extraction,
+    Table_Apidae.apidae,
     Table_category.category,
     Table_profil.user_profil,
-    Table_selection.selection,
-    Table_ManualEntry.manualEntry,
-    Table_freshness.freshness_level,
+    Table_admin.admin,
     Table_elementReference.elementRef,
-    Table_extraction.selection_extraction,
+    Table_relation_selection_category.relation_selection_category,
     Table_relation_selection_profil.relation_selection_profil,
-    Table_relation_selection_category.relation_selection_category
+    Table_ManualEntry.manualEntry,
+    Table_freshness.freshness_level
 )
 
 if __name__ == "__main__":

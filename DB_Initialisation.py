c32c3b685642b768322aac8c096aec1ec2293cd4
Zahra Dubeau <zdub@dubz.com>
2020-07-10 16:22:45 +0200

    Replace some tables & Add some

--- DB_Initialisation.py
+++ DB_Initialisation.py
@@ -16,6 +16,8 @@ import Table_ManualEntry
 import Table_elementReference
 import Table_relation_selection_profil
 import Table_relation_selection_category
+import Table_relation_eltref_prf
+import Table_relation_eltRef_ctg
 from DB_Connexion import DB_connexion
 # _____________________________________________
 
@@ -25,12 +27,14 @@ full_actions_list = (
     Table_selection.drop_selection,
     Table_extraction.drop_selection_extraction,
     Table_Apidae.drop_apidae,
-    Table_admin.drop_admin,
     Table_category.drop_category,
     Table_profil.drop_user_profil,
     Table_elementReference.drop_elementRef,
-    Table_relation_selection_category.drop_relation_selection_category,
+    Table_relation_eltref_prf.drop_relation_eltref_profil,
+    Table_relation_eltRef_ctg.drop_relation_eltref_category,
     Table_relation_selection_profil.drop_relation_selection_profil,
+    Table_relation_selection_category.drop_relation_selection_category,
+    Table_admin.drop_admin,
     Table_ManualEntry.drop_manualEntry,
     Table_freshness.drop_freshness_level,
     Table_message.message,
@@ -40,10 +44,12 @@ full_actions_list = (
     Table_Apidae.apidae,
     Table_category.category,
     Table_profil.user_profil,
-    Table_admin.admin,
     Table_elementReference.elementRef,
-    Table_relation_selection_category.relation_selection_category,
+    Table_relation_eltref_prf.relation_eltref_profil,
+    Table_relation_eltRef_ctg.relation_eltref_category,
     Table_relation_selection_profil.relation_selection_profil,
+    Table_relation_selection_category.relation_selection_category,
+    Table_admin.admin,
     Table_ManualEntry.manualEntry,
     Table_freshness.freshness_level
 )
